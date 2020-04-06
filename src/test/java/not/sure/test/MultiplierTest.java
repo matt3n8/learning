f4c007f9eb8da49de24da15663d653af8ffadff3
Matt Benton <mbenton@wcute.af.mil>
2020-04-06 13:41:37 -0500

    trying to add tests

---
+++ src/test/java/not/sure/test/MultiplierTest.java
@@ -0,0 +1,36 @@
+package not.sure.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+public class MultiplierTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCompute() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testMultiplier() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
---
+++ src/test/java/not/sure/test/OperatorTest.java
@@ -0,0 +1,47 @@
+package not.sure.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class OperatorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCompute() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testOperator() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetResult() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testPrintResult() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
