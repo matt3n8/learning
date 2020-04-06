eb664c0d1fa485d5e474f0ccfdea48f256a1d3df
Matt Benton <mbenton@wcute.af.mil>
2020-04-06 10:25:03 -0500

    Added exponentiation

---
+++ src/main/java/not/sure/Exponentiate.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package not.sure;
+
+/**
+ * @author Matt
+ *
+ */
+public class Exponentiate extends Math {
+
+	public Exponentiate(double first, double second) {
+		super(first, second);
+	}
+
+	public void compute() {
+		result = firstValue * secondValue;
+
+	}
+
+}
